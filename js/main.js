c6697108b3ff2ac9d5b9a780675cbd00655f3d93
AimlessFrostbite <kristida84@gmail.com>
2019-10-03 22:58:25 -0700

    adds basic styling and further button functionality

--- js/main.js
+++ js/main.js
@@ -1,12 +1,34 @@
 const income = document.querySelector('.income');
 const calculate = document.querySelector('.calculate');
 const budgetDisplay = document.querySelector('.bDisplay');
+const section = document.querySelector('.gallery-section');
 
+
+//find a house button effect
 calculate.addEventListener('click', display => {
-    budgetDisplay.textContent = budget();
+    budgetDisplay.textContent = '$' + budget();
+    section.classList.remove('budget');
 });
 
 function budget() {
  const payment = ((income.value * .25)*12)*15;
  return payment;
 }
+
+//image gallery functions
+function imageGallery() {
+    const highlight = document.querySelector('.gallery-highlight');
+    const previews = document.querySelectorAll('.house-preview img');
+
+    previews.forEach(preview =>{
+        preview.addEventListener('click', function() {
+            const smallSrc = this.src;
+            const bigSrc = smallSrc.replace('small', 'big');
+            highlight.src = bigSrc;
+            previews.forEach(preview => preview.classList.remove('house-active'));
+            preview.classList.add('house-active');
+        });
+    });
+}
+
+imageGallery();
